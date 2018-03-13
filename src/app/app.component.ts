769802e6e73c1b2532da87b148c0e54f7c2d64f9
Aleksei Rogozin <alrogozin@gmail.com>
2018-03-13 16:16:15 +0300

    Initial.Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Recipe } from './model/recipe';
 
 @Component({
   selector: 'app-root',
-templateUrl: './app.component.html',
-styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
 
+export class AppComponent {
+  recipes: Recipe[];
+  constructor() {
+    this.recipes = [
+      new Recipe('Блинчики', 'Любимые блинчики на молоке и масле.', null, null, null),
+      new Recipe('Шашлык', 'Из свинной шейки на углях.', null, null, null),
+    ];
+  }
 }
